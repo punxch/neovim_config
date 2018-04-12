344e029b2a566a4e201967be7a9736aaf40da3f4
Wesley <punxch@gmail.com>
2018-04-13 01:00:33 +0800

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 vimrc_path=~/.config/nvim/
 test -d "$vimrc_path" || mkdir -p "$vimrc_path" && \
 cp -r init.vim $vimrc_path && \
-cp -R colors/ $vimrc_path
+cp -R colors $vimrc_path
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
